66336c949688d9678fe166c1f3aaa8ea9acb5f4c
gui.massoqueto <gui.massoqueto@gmail.com>
2022-07-27 19:32:13 -0300

    update: SQLAlchemy CRUD

--- app_sqlalchemy/main.py
+++ app_sqlalchemy/main.py
@@ -1,48 +1,40 @@
-from turtle import title
 from fastapi import FastAPI, Depends, Response, status, HTTPException
-from requests import delete
-from . import models
-from .database import engine, SessionLocal, get_db
+from . import models, schemas
+from .database import engine, get_db
 from sqlalchemy.orm import Session
-from .schemas import Post
+from typing import List
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-@app.get("/sqlalchemy/posts")
+@app.get("/sqlalchemy/posts", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
 
-    return {
-        "data": posts
-    }
+    return posts
 
 
-@app.post("/sqlalchemy/posts", status_code=status.HTTP_201_CREATED)
-def post_posts(post: Post, db: Session = Depends(get_db)):
+@app.post("/sqlalchemy/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+def post_posts(post: schemas.CreatePost, db: Session = Depends(get_db)):
     new_post = models.Post(**post.dict())
 
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
 
-    return {
-        "data": new_post
-    }
+    return new_post
 
 
-@app.get("/sqlalchemy/posts/{id}", status_code=status.HTTP_200_OK)
+@app.get("/sqlalchemy/posts/{id}", status_code=status.HTTP_200_OK, response_model=schemas.Post)
 def post_post(id: int, db: Session = Depends(get_db)):
-    post_from_db = db.query(models.Post).filter(models.Post.id == id).all()
+    single_post = db.query(models.Post).filter(models.Post.id == id).first()
     
-    if not post_from_db:
+    if not single_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id={id} not found")
 
-    return {
-        "data": post_from_db
-    }
+    return single_post
 
 
 @app.delete("/sqlalchemy/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -58,20 +50,17 @@ def delete_post(id: int,  db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/sqlalchemy/posts/{id}", status_code=status.HTTP_202_ACCEPTED)
-def put_post(id: int, post: Post, db: Session = Depends(get_db)):
-    post_to_change = db.query(models.Post).filter(models.Post.id == id)
+@app.put("/sqlalchemy/posts/{id}", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.Post)
+def put_post(id: int, post: schemas.UpdatePost, db: Session = Depends(get_db)):
+    changed_post = db.query(models.Post).filter(models.Post.id == id)
 
-    if not post_to_change.all(): 
+    if not changed_post: 
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post {id} was not found"
         )
 
-    post_to_change.update(post.dict(), synchronize_session=False)
+    changed_post.update(post.dict(), synchronize_session=False)
     db.commit()
 
-    return {
-        "message": "post updated",
-        "data": post_to_change.all()
-    }
+    return changed_post.first()
--- app_sqlalchemy/schemas.py
+++ app_sqlalchemy/schemas.py
@@ -1,8 +1,23 @@
+from datetime import datetime
 from pydantic import BaseModel
 from typing import Optional
 
 
-class Post(BaseModel):
+class BasePost(BaseModel):
     title: str
     content: Optional[str] = None
     published: Optional[bool] = True
+
+
+class CreatePost(BasePost):
+    pass
+
+
+class UpdatePost(BasePost):
+    pass
+
+class Post(BasePost):
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
