84b9eb7823e017ade75e4250013b34d877528edb
gui.massoqueto <gui.massoqueto@gmail.com>
2022-07-27 10:34:53 -0300

    update: minor bug fixes

--- app_sqlalchemy/main.py
+++ app_sqlalchemy/main.py
@@ -1,22 +1,15 @@
 from fastapi import FastAPI, Depends
 from . import models
-from .database import engine, SessionLocal
+from .database import engine, SessionLocal, get_db
 from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
 @app.get("/sqlalchemy")
 def get_something(db: Session = Depends(get_db)):
+
     return {
         "balada": "top"
     }
